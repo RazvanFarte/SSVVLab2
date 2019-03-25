5d32265ca2f08f8be6c19ea38734947f95cdd06a
Razvan Farte <farterazvan@gmail.com>
2019-03-25 16:40:59 +0200

    Tests

--- src/test/java/Repository/MemoryRepository/StudentRepoTest.java
+++ src/test/java/Repository/MemoryRepository/StudentRepoTest.java
@@ -1,4 +1,29 @@
 package Repository.MemoryRepository;
 
+import domain.Student;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import repository.StudentRepository;
+import validation.StudentValidator;
+
 public class StudentRepoTest {
+
+    StudentRepository studentRepository;
+
+    @Before
+    public void setUp() {
+        studentRepository = new StudentRepository(new StudentValidator());
+
+        studentRepository.save(new Student("1", "1", 1));
+    }
+
+    @Test
+    public void testGet() {
+        Student student = studentRepository.findOne("1");
+
+    }
+
 }
+
+
